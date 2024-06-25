ce531b251a18a41f4cdf12c8e9d92bc5f86c4a37
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 18:00:05 +0600

    Question1 displayed in Question box

--- src/components/Question1.js
+++ src/components/Question1.js
@@ -1,9 +1,14 @@
+import React, {useState} from "react";
+import QuestionBox from "./QuestionBox";
+
 const Question1 = () => {
+  const [question, setQuestion] = useState(
+    "Question-1: What is the mean (average) of the following set of numbers: 4, 8, 15, 16, 23, 42?" 
+  )
+
   return (
       <>
-        <div>            
-            Question-1: What is the mean (average) of the following set of numbers: 4, 8, 15, 16, 23, 42?         
-        </div>        
+        <QuestionBox question={question} setQuestion={setQuestion} />       
       </>
   );
 };
