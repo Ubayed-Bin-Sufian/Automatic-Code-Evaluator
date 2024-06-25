45ebd1fdea5168db2c683e92eb263b363b277fd2
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 11:58:51 +0600

    Test: An actual question 2 added

--- src/components/Question2.js
+++ src/components/Question2.js
@@ -5,9 +5,8 @@ const Question2 = () => {
   const navigate = useNavigate();
   return (
       <>
-        <div>
-            <h1>Question - 2</h1>
-            <p>Question 2 is displayed here</p>
+        <div>            
+            <p>Question - 2: What is the median of the numbers 1, 3, 3, 6, 7, 8, 9?</p>
             <button onClick={() => navigate(-1)}>Back</button>
         </div>        
       </>
