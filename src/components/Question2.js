e30642ec9177b9dd8be049510f8328123e66b5ce
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 12:53:34 +0600

    Back button removed from question 2

--- src/components/Question2.js
+++ src/components/Question2.js
@@ -1,13 +1,8 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
 const Question2 = () => {
-  const navigate = useNavigate();
   return (
       <>
         <div>            
-            <p>Question - 2: What is the median of the numbers 1, 3, 3, 6, 7, 8, 9?</p>
-            <button onClick={() => navigate(-1)}>Back</button>
+            Question - 2: What is the median of the numbers 1, 3, 3, 6, 7, 8, 9?
         </div>        
       </>
   );
