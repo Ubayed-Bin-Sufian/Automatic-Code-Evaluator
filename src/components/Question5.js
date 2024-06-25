ec993a1bf48936ecf212989da30ecd241c302e51
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 18:02:26 +0600

    Question5 displayed in Question box

--- src/components/Question5.js
+++ src/components/Question5.js
@@ -1,9 +1,14 @@
+import React, {useState} from "react";
+import QuestionBox from "./QuestionBox";
+
 const Question5 = () => {
+  const [question, setQuestion] = useState(
+    "Question - 5: What is a scatter plot and when would you use it?" 
+  )
+
   return (
       <>
-        <div>
-            Question - 5: What is a scatter plot and when would you use it?
-        </div>        
+        <QuestionBox question={question} setQuestion={setQuestion} />       
       </>
   );
 };
