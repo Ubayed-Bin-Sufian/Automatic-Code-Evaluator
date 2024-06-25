8f91943ec827957849be6514e6f4df1d14325d0f
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 12:53:41 +0600

    Back button removed from question 3

--- src/components/Question3.js
+++ src/components/Question3.js
@@ -1,13 +1,8 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
 const Question3 = () => {
-  const navigate = useNavigate();
   return (
       <>
         <div>
-            <p>Question - 3: How do you find the mode of the following set of numbers: 1, 2, 2, 3, 4, 4, 4, 5?</p>
-            <button onClick={() => navigate(-1)}>Back</button>
+            Question - 3: How do you find the mode of the following set of numbers: 1, 2, 2, 3, 4, 4, 4, 5?
         </div>        
       </>
   );
