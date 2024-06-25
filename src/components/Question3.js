6186078ff7c6af4a55236a4bf3b1b5809c5c8e3b
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 18:01:34 +0600

    Question3 displayed in Question box

--- src/components/Question3.js
+++ src/components/Question3.js
@@ -1,9 +1,14 @@
+import React, {useState} from "react";
+import QuestionBox from "./QuestionBox";
+
 const Question3 = () => {
+  const [question, setQuestion] = useState(
+    "Question - 3: How do you find the mode of the following set of numbers: 1, 2, 2, 3, 4, 4, 4, 5?" 
+  )
+
   return (
       <>
-        <div>
-            Question - 3: How do you find the mode of the following set of numbers: 1, 2, 2, 3, 4, 4, 4, 5?
-        </div>        
+        <QuestionBox question={question} setQuestion={setQuestion} />       
       </>
   );
 };
