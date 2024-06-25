d8e8bfc4195faa6e53a5017bf56cc3c1e95ef4a9
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 12:53:25 +0600

    Back button removed from question 1

--- src/components/Question1.js
+++ src/components/Question1.js
@@ -1,13 +1,8 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
 const Question1 = () => {
-  const navigate = useNavigate();
   return (
       <>
         <div>            
-            <p>Question-1: What is the mean (average) of the following set of numbers: 4, 8, 15, 16, 23, 42?</p>
-            <button onClick={() => navigate(-1)}>Back</button>
+            Question-1: What is the mean (average) of the following set of numbers: 4, 8, 15, 16, 23, 42?         
         </div>        
       </>
   );
