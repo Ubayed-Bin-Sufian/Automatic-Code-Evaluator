52568f9bb4e9f5f72baa21c19901958c09e965ad
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-25 18:01:03 +0600

    Question2 displayed in Question box

--- src/components/Question2.js
+++ src/components/Question2.js
@@ -1,9 +1,14 @@
+import React, {useState} from "react";
+import QuestionBox from "./QuestionBox";
+
 const Question2 = () => {
+  const [question, setQuestion] = useState(
+    "Question - 2: What is the median of the numbers 1, 3, 3, 6, 7, 8, 9?" 
+  )
+
   return (
       <>
-        <div>            
-            Question - 2: What is the median of the numbers 1, 3, 3, 6, 7, 8, 9?
-        </div>        
+        <QuestionBox question={question} setQuestion={setQuestion} />       
       </>
   );
 };
