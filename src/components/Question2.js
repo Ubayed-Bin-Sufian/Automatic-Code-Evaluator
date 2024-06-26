f835a7902aecb2c333ded1d5fbd2a82b017d15bd
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-26 13:14:06 +0600

    import ques2 from JSON and map it with ques2 comp

--- src/components/Question2.js
+++ src/components/Question2.js
@@ -1,14 +1,11 @@
-import React, {useState} from "react";
 import QuestionBox from "./QuestionBox";
+import questions from "../data/questions.json"
 
 const Question2 = () => {
-  const [question, setQuestion] = useState(
-    "Question - 2: What is the median of the numbers 1, 3, 3, 6, 7, 8, 9?" 
-  )
 
   return (
       <>
-        <QuestionBox question={question} setQuestion={setQuestion} />       
+        <QuestionBox question={questions[1].Question2} />       
       </>
   );
 };
