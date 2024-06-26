d3e1280b62cd2d6245c63b6cc43258558bdfeda5
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-26 13:14:34 +0600

    import ques4 from JSON and map it with ques4 comp

--- src/components/Question4.js
+++ src/components/Question4.js
@@ -1,14 +1,11 @@
-import React, {useState} from "react";
 import QuestionBox from "./QuestionBox";
+import questions from "../data/questions.json"
 
 const Question4 = () => {
-  const [question, setQuestion] = useState(
-    "Question - 4: What is a histogram and how is it used?" 
-  )
 
   return (
       <>
-        <QuestionBox question={question} setQuestion={setQuestion} />       
+        <QuestionBox question={questions[3].Question4} />       
       </>
   );
 };
