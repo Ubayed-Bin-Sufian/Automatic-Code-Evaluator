f88a4d3c1790d5e4f7ee15fa0a3050165cf13be5
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-26 13:13:47 +0600

    import ques1 from JSON and map it with ques1 comp

--- src/components/Question1.js
+++ src/components/Question1.js
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from "react";
 import QuestionBox from "./QuestionBox";
 import questions from "../data/questions.json"
 
 const Question1 = () => {
-  const [question, setQuestion] = useState(
-    "Question-1: What is the mean (average) of the following set of numbers: 4, 8, 15, 16, 23, 42?" 
-  )
 
   return (
       <>
-        <QuestionBox question={questions[0].Question1} setQuestion={setQuestion} />       
+        <QuestionBox question={questions[0].Question1} />       
       </>
   );
 };
