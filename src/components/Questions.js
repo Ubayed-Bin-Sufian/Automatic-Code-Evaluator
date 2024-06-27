20a74686b3eafd3fa6ea6b6ca532bca860dfc0d8
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-28 02:29:24 +0600

    Get questionID from URL, Convert questionID to index, Create key string, Retrieve question from JSON.

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,16 +1,24 @@
+// Get questionID from current URL, Convert questionID to index, Create key string and retrieves question from JSON.
+
 import QuestionBox from "./QuestionBox";
 import questions from "../data/questions.json"
 import { useParams } from "react-router-dom";
 
 const Questions = () => {
 
-  const {questionID} = useParams();
-  console.log(questionID);
+  const { questionID } = useParams();  // Destructure the questionID from useParams
+  console.log({ questionID });  // For debuggin purpose
+
+  const questionIndex = parseInt(questionID) - 1; // Convert questionID (which is a string) to an index (an integer)
+  const questionKey = `Question${questionID}`;  // Uses template literals to construct a string
+  const question = questions[questionIndex][questionKey];
+  
 
   return (
-      <>
-        <QuestionBox question={questions[0].Question1} />             
-      </>      
+      <div>
+        {/* <QuestionBox question={questions[0].Question1} /> */}
+        <QuestionBox question={question} />       
+      </div>      
   );
 };
 
