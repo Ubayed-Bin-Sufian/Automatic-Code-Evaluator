cc91d99cd5cd72ec9fd2bba84b05f527f74185f4
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-02 21:19:16 +0600

    Unnecessary parts such as  white spaces removed

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -3,8 +3,6 @@ import { classnames } from "../utils/general";
 
 const CustomInput = ({ customInput, setCustomInput }) => {
   return (
-    <>
-      {" "}
       <textarea
         rows="5"
         value={customInput}
@@ -14,8 +12,7 @@ const CustomInput = ({ customInput, setCustomInput }) => {
           "focus:outline-none w-full border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white mt-2"
         )}
       ></textarea>
-    </>
-  );
+    );
 };
 
 export default CustomInput;
