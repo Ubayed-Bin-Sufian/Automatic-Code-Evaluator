fb139488300a9c414b8ce2408d2cba5e68933fff
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-04 17:44:02 +0600

    parameter name changed and 'Accepted' color changed to green

--- src/components/OutputDetailsTestCases.js
+++ src/components/OutputDetailsTestCases.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-const OutputDetailsTestCases = ({ outputDetails }) => {
+const OutputDetailsTestCases = ({ OutputDetailsTestCases }) => {
   return (
     <div className="metrics-container mt-4 flex flex-col space-y-3">
       <p className="text-sm">
         Status:{" "}
-        <span className="font-semibold px-2 py-1 rounded-md bg-gray-100">
-          {outputDetails?.status?.description}
+        <span className="font-semibold text-green-500 px-2 py-1 rounded-md bg-gray-100">
+          {OutputDetailsTestCases?.status?.description}
         </span>
       </p>
       <p className="text-sm">
         Memory:{" "}
         <span className="font-semibold px-2 py-1 rounded-md bg-gray-100">
-          {outputDetails?.memory}
+          {OutputDetailsTestCases?.memory}
         </span>
       </p>
       <p className="text-sm">
         Time:{" "}
         <span className="font-semibold px-2 py-1 rounded-md bg-gray-100">
-          {outputDetails?.time}
+          {OutputDetailsTestCases?.time}s
         </span>
       </p>
     </div>
