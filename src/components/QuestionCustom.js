1e377af688b0d37f13a5eae45d5dab918fa0746e
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-17 20:40:46 +0600

    Typo-Fix-#2

--- src/components/QuestionCustom.js
+++ src/components/QuestionCustom.js
@@ -1,10 +1,6 @@
-// Get questionID from current URL, Convert questionID to index, Create key string and retrieves question from JSON.
-
 import QuestionBox from "./QuestionBox";
 
 const QuestionCustom = (question) => {
-
-
   return (
       <div>
         <QuestionBox question={question.question} />       
