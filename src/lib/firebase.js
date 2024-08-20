677177edb2c88c8bf49f84a37ca42454c7dcbd69
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-08-20 20:21:03 +0600

    fetch values from env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,27 +1,24 @@
-import {initializeApp} from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-import { getFirestore, doc, setDoc, getDoc, writeBatch, collection, getDocs, onSnapshot, deleteDoc } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDoc, getDocs, getFirestore, onSnapshot, setDoc, writeBatch } from 'firebase/firestore';
 
-const firebaseConfig ={
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "codeevaluator.firebaseapp.com",
-    projectId: "codeevaluator",
-    storageBucket: "codeevaluator.appspot.com",
-    messagingSenderId: "252590889208",
-    appId: "1:252590889208:web:415907f7d1a8f6a47cc30d"
-  };
-  
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};  
 
-//let firebase = null
+// let firebase = null
 const firebase = initializeApp(firebaseConfig);
 
 // Auth exports
 const auth = getAuth();
 const googleAuthProvider = new GoogleAuthProvider();
 
-// // Firestore exports
-//const firestore = getDatabase();
+// Firestore exports
 const firestore = getFirestore(firebase);
 
-export { auth, googleAuthProvider, signInWithPopup, firestore, doc, setDoc, getDoc, writeBatch, collection, getDocs, onSnapshot, signOut, deleteDoc  };
+export { auth, collection, deleteDoc, doc, firestore, getDoc, getDocs, googleAuthProvider, onSnapshot, setDoc, signInWithPopup, signOut, writeBatch };
